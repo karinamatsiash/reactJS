dd37bbd2c812903c16b20503f07efb6f82220020
Karyna Matsiash <krivitskayakarina@gmail.com>
2025-03-20 15:59:18 +0100

    [Module 1] Fixe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
